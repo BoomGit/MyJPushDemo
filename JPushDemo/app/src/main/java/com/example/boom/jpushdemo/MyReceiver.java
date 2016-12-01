26ffe160381a233825d8ed14f216656cfb3b3c2a
Boom <631903189@qq.com>
2016-12-01 22:54:40 +0800

    添加集成极光推送小例子的文件

---
+++ JPushDemo/app/src/main/java/com/example/boom/jpushdemo/MyReceiver.java
@@ -0,0 +1,15 @@
+package com.example.boom.jpushdemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Boom on 2016/12/1.
+ */
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
